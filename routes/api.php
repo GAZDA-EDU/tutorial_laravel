b9958a1759e00720d7ab838dcf479c674cdbe7c6
GAZDA-EDU <gazda.edu@gmail.com>
2024-10-11 18:14:46 +0200

    Making our First Request

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return 'hello from the api route';
 });
+
+Route::get('posts', [TestController::class, 'index']);
