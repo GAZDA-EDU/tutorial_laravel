ed63d74ebe998423739fe873222e7ae53c13cd56
GAZDA-EDU <gazda.edu@gmail.com>
2024-10-18 19:17:45 +0200

    Token Based Authentication In Laravel -1

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthTokenControlller;
 use App\Http\Controllers\TestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return 'hello from the api route';
-});
+Route::post('login', [AuthTokenControlller::class, 'login']);
+Route::post('register', [AuthTokenControlller::class, 'register']);
 
-Route::get('posts', [TestController::class, 'index']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
